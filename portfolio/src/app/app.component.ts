68658c256dd0556914d061812fb5e77cd9cfbc68
Dhiviya Bharathi <dhiviarul@gmail.com>
2024-05-14 21:05:23 +0200

    Theme and fonts added

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
