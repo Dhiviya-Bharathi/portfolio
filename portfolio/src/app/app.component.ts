5d7935b32b270d53a9bf1243fa1bd45401eea08f
dhilip_backbase <dhilip@backbase.com>
2024-05-16 22:02:40 +0200

    added font awesome

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: HeaderComponent },
@@ -11,7 +12,7 @@ const routes: Routes = [
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, HeaderComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule, HeaderComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfolio/src/app/skills/skills.component.ts
+++ portfolio/src/app/skills/skills.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
 
+// CERTIFICATIONS HELD
+// AZ900 AZURE FUNDAMENTALS
+// AZ 203 DEVELOPING AZURE SOLUTIONS
+export class SkillsComponent {
+  techSkills = [
+    {
+      category: 'Frameworks & UX',
+      items: [
+        { name: 'Angular', icon: 'angular' },
+        { name: 'Vuejs', icon: 'angular' },
+        { name: 'BOOTSTRAP 4X', icon: 'angular' },
+      ],
+    },
+    {
+      category: 'Languages',
+      items: [
+        { name: 'HTML 5', icon: 'angular' },
+        { name: 'CSS 3', icon: 'angular' },
+        { name: 'JAVASCRIPT', icon: 'angular' },
+        { name: 'PYTHON', icon: 'angular' },
+      ],
+    },
+    {
+      category: 'Deployment & Monitoring',
+      items: [
+        { name: 'DOCKER', icon: 'angular' },
+        { name: 'KUBERNETIS', icon: 'angular' },
+        { name: 'APPSERVICE', icon: 'angular' },
+        { name: 'AWS S3', icon: 'angular' },
+        { name: 'PM2', icon: 'angular' },
+      ],
+    },
+    {
+      category: 'Databases',
+      items: [
+        { name: 'POSTGRES', icon: 'angular' },
+        { name: 'SQL', icon: 'angular' },
+        { name: 'ORACLE SQL', icon: 'angular' },
+        { name: 'AZURE STORAGE ADLS', icon: 'angular' },
+      ],
+    },
+  ];
 }
